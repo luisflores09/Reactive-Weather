1beada59278c2a46c288565cef9a957849c9fe4c
Luis <iluisflores@yahoo.com>
2025-03-17 14:19:34 -0500

    in App.js I mapped over the cities array using WeatherCard component

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
 // Import data and WeatherCard here
-
+import WeatherCard from "./components/WeatherCard.js";
+import cities from "./data.js";
 
 function App() {
-    return (
-        <>
-            <h1 className = "title">REACTIVE WEATHER</h1>
-            <h3 className = "subtitle">Up to the minute weather news</h3>
-            <div className = "app">
-                {/* Render components here */}
-                
-
-
-            </div>
-            
-        </>
-    )
+  return (
+    <>
+      <h1 className="title">REACTIVE WEATHER</h1>
+      <h3 className="subtitle">Up to the minute weather news</h3>
+      <div className="app">
+        {cities.map((city, index) => (
+          <WeatherCard
+            key={index}
+            city={city.city}
+            temperature={city.temperature}
+            forecast={city.forecast}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
